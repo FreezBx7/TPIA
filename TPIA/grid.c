580950bad31872f9bd932ab13e72cbadae2b3021
Cécile CROCHON <cecile.crochon@progress-it.fr>
2019-12-18 09:19:55 +0100

    (fix) print grid

--- TPIA/grid.c
+++ TPIA/grid.c
@@ -1,8 +1,9 @@
 #include "grid.h"
+#include <stdlib.h>
+#include <assert.h>
 
 GRID init_grid(int size_grid) {
     GRID g;
-    int middle = 0;
     if(size_grid > 0 && (size_grid%2 == 0)) {
         g.size_grid = size_grid;
     } else {
@@ -11,14 +12,14 @@ GRID init_grid(int size_grid) {
     }
     g.board = (int **)calloc(g.size_grid, sizeof(int *));
     assert(g.board != NULL);
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < g.size_grid; i++) {
         g.board[i] = (int *)calloc(g.size_grid, sizeof(int));
         assert(g.board[i] != NULL);
     }
     g.nb_white = 0;
     g.nb_black = 0;
 
-    middle = g.size-grid/2;
+    int middle = (int) g.size_grid/2;
     g.board[middle-1][middle-1] = 'W';
     g.board[middle][middle] = 'W';
     g.board[middle-1][middle] = 'B';
@@ -31,7 +32,7 @@ GRID pointCounter (GRID g) {
     g.nb_white = 0;
     g.nb_black = 0;
     for(int i = 0; i < g.size_grid; i++) {
-        for(int j = 0, j < g.size_grid; j++) {
+        for(int j = 0; j < g.size_grid; j++) {
             if(g.board[i][j] == 'B') {
                 g.nb_black++;
             } else {
@@ -47,9 +48,13 @@ GRID pointCounter (GRID g) {
 void print_GRID(GRID g) {
     for(int i = 0; i<g.size_grid; i++) {
         for(int j = 0; j<g.size_grid; j++) {
-            printf("|%c", g.board[i][j]);
+                if(g.board[i][j]) {
+                    printf("|%c ", g.board[i][j]);
+                } else {
+                    printf("|  ", g.board[i][j]);
+                }
         }
         printf("|\n");
     }
-    printf("-----------------------")
+    printf("-----------------------");
 }
--- TPIA/main.c
+++ TPIA/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "grid.h"
 
 int main()
 {
     printf("Hello world!\n");
+    GRID g = init_grid(6);
+    print_GRID(g);
     return 0;
 }
